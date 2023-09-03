2f60db33b259b49dce5f31da40c321355f528e27
Ngozi89 <>
2023-09-03 14:32:03 +0000

    Add game play board and contros

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let playBoard = document.querySelector(".play-board");
+let scoreElement = document.querySelector(".score");
+let highScoreElement = document.querySelector(".high-score");
+let controls = document.querySelectorAll(".controls i");
