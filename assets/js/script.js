c9b571c38a2bdb8f1c076270a5fee78f6828c4b3
Ngozi89 <>
2023-09-03 15:11:07 +0000

    Add two random numbers for snake food

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ let controls = document.querySelectorAll(".controls i");
 
 let foodA = 15, foodB = 12;
 
+//Creates two random numbers between 1 and 20
+foodA = Math.floor(Math.random() * 20) + 1;
+foodB = Math.floor(Math.random() * 20) + 1;
+
 let initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodA} / ${foodB}"></div>`;
     playBoard.innerHTML = htmlMarkup;
