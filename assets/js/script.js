361ffbc03e6ec8fb044a25f92f484c00d43eabca
Ngozi89 <>
2023-09-03 14:59:41 +0000

    Set up snake food

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ let playBoard = document.querySelector(".play-board");
 let scoreElement = document.querySelector(".score");
 let highScoreElement = document.querySelector(".high-score");
 let controls = document.querySelectorAll(".controls i");
+
+let foodA = 15, foodB = 12;
+
+let initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodA} / ${foodB}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+}
+initGame();
